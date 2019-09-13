820bc798c59d8dc838ea169d6d142372244cc007
Steviecoaster <stephen@chocolatey.io>
2019-09-13 02:52:27 +0000

    [bob] can we fix it?!

---
+++ helpers.ps1
@@ -0,0 +1,24 @@
+function Get-WorkingPackage {
+    
+    $commit = git log -1 --pretty=%B
+    $null = $commit[0] -match '(?<package>(?<=\[).+?(?=\]))'
+
+    return $matches.package
+
+}
+
+function Initialize-ChocoPackage {
+    
+    $package = Get-WorkingPackage
+    $nuspec = Get-ChildItem $env:GITHUB_WORKSPACE -Recurse -Filter '*.nuspec' | Where-Object { $_.BaseName -eq $package }
+    choco pack $nuspec.FullName --output-directory="'$($env:GITHUB_WORKSPACE)'"
+
+}
+
+Function Publish-ChocoPackage {
+
+    $package = Get-WorkingPackage
+    $nupkg = Get-ChildItem $env:GITHUB_WORKSPACE -Recurse -Filter '*.nupkg' | 
+    Where-Object { $_.Basename -eq $package } | 
+    Foreach-Object { choco push $_.FullName --source="'Github'" }
+}
--- publish.ps1
+++ publish.ps1
@@ -1,1 +1,5 @@
+. ./helpers.ps1
+
 choco source Add --name="'GitHub'" --source="'https://nuget.pkg.github.com/steviecoaster/ActionsTest/index.json'" -UserName="'steviecoaster'" --Password="'$env:TOKEN'"
+
+Publish-ChocoPackage
