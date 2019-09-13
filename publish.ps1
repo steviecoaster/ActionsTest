67768774ec80ce0f387f2261232bdbc898f4266b
Steviecoaster <stephen@chocolatey.io>
2019-09-13 03:18:17 +0000

    [bob] let's add some logging and see what we can dig up

--- publish.ps1
+++ publish.ps1
@@ -3,3 +3,5 @@
 choco source Add --name="'GitHub'" --source="'https://nuget.pkg.github.com/steviecoaster/ActionsTest/index.json'" -UserName="'steviecoaster'" --Password="'$env:TOKEN'"
 choco apikey -k "steviecoaster:$($env:TOKEN)" -s https://nuget.pkg.github.com/steviecoaster/ActionsTest/index.json
 Publish-ChocoPackage
+
+Get-Content "$env:ChocolateyInstall\logs\chocolatey.log"
