c1fab7ded2acd12faf8d95a815bdf398ad191a4c
Steviecoaster <stephen@chocolatey.io>
2019-09-13 03:06:53 +0000

    [bob] I'm gonna need bourbon.

--- publish.ps1
+++ publish.ps1
@@ -1,5 +1,5 @@
 . ./helpers.ps1
 
 choco source Add --name="'GitHub'" --source="'https://nuget.pkg.github.com/steviecoaster/ActionsTest/index.json'" -UserName="'steviecoaster'" --Password="'$env:TOKEN'"
-
+choco apikey -k "steviecoaster:$($env:TOKEN)" -s https://nuget.pkg.github.com/steviecoaster/ActionsTest/index.json
 Publish-ChocoPackage
